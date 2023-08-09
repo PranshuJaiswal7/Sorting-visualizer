1e895a62b6ae298ee78b0e26b5245dcb3bb58c4e
PranshuJaiswal <2021ucs0107@iitjammu.ac.in>
2023-08-09 17:03:00 +0530

    Added insertion sort

--- javascript/script.js
+++ javascript/script.js
@@ -2,11 +2,13 @@ let random=document.getElementById("random");
 let sort=document.getElementById("sort");
 let bars_container=document.getElementById("bars_container")
 let slider = document.getElementById("slider");
+let select_algo = document.getElementById("algo");
+let algotouse="bubble";
 let minrange=1;
 let maxrange=slider.value;
 let n=slider.value;
 let heightFactor = 4;
-let speedFactor = 100;
+let speedFactor = 1000;
 let unsorted_array=new Array(n);
 function random_number(min,max){
     return Math.floor(Math.random()*(max-min+1))+min;
@@ -26,6 +28,9 @@ speed.addEventListener("change", (e) => {
   speedFactor = parseInt(e.target.value);
 });
 
+select_algo.addEventListener("change", function () {
+  algotouse = select_algo.value;
+});
 
 
 slider.addEventListener("input", function () {
@@ -45,6 +50,7 @@ document.addEventListener("DOMContentLoaded",function(){
 
 
 function changebar(array){
+   
     for(let i=0;i<array.length;i++){
     let bar=document.createElement("div");
     bar.classList.add("bar")
@@ -63,6 +69,10 @@ function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
   
+
+
+
+  //bubble sort
   async function bubbleSort(array) {
     let bars = document.getElementsByClassName("bar");
     for (let i = 0; i < array.length; i++) {
@@ -87,11 +97,104 @@ function sleep(ms) {
       }
       await sleep(speedFactor);
     }
+
+    for (let k = 0; k < bars.length; k++) {
+      bars[k].style.backgroundColor = "aqua";
+    }
     return array;
   }
 
 
-  sort.addEventListener("click",function(){
-    bubbleSort(unsorted_array);
-  })
+//Insertion sort
+
+async function InsertionSort(array) {
+  let bars = document.getElementsByClassName("bar");
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > key) { 
+      
+      let temp=array[j+1];
+      array[j + 1] = array[j];
+      array[j]=temp;
+      bars[j + 1].style.backgroundColor = "red";
+      bars[j].style.backgroundColor = "lightgreen"; 
+      bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
+     bars[j].style.height = array[j ] * heightFactor + "px";
+      // bars[j + 1].innerText = array[j + 1];
+      await sleep(speedFactor);
+
+      for (let k = 0; k < bars.length; k++) {
+        if (k != j + 1 && k!=j) {
+          bars[k].style.backgroundColor = "aqua";
+        }
+      }
+      j = j - 1;
+    }
+    
+   
+    // bars[j + 1].innerText = array[j + 1];
+    await sleep(speedFactor);
+  }
+
+  for (let k = 0; k < bars.length; k++) {
+    bars[k].style.backgroundColor = "aqua";
+  }
+  return array;
+}
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//slecting the algo
+
+
+ sort.addEventListener("click", function () {
+  switch (algotouse) {
+    case "bubble":
+      bubbleSort(unsorted_array);
+      break;
+    // case "merge":
+    //   if (
+    //     confirm(
+    //       "Merge Sort is not visualized properly. Do you want to continue?"
+    //     )
+    //   ) {
+    //     mergeSort(unsorted_array);
+    //   } else {
+    //     break;
+    //   }
+    //   //console.log(mergeSort(unsorted_array));
+    //   break;
+    // case "heap":
+    //   HeapSort(unsorted_array);
+    //   break;
+    case "insertion":
+      InsertionSort(unsorted_array);
+      break;
+    // case "quick":
+    //   console.log(unsorted_array.length);
+
+    //   quickSort(unsorted_array, 0, unsorted_array.length - 1);
+    //   break;
+    default:
+      bubbleSort(unsorted_array);
+      break;
+  }
+});
+
+
+
+
 
